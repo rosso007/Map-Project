35c8704ee36ceca03730427bfe836151a8cfccc9
Ross Drury <ross.drury@gmail.com>
2016-01-10 18:44:37 +1300

    Update to requesting data, markers and infowindows.

--- js/newapp.js
+++ js/newapp.js
@@ -1,61 +1,27 @@
-var initialMarkers = [
-	{ 
-			"name": "Museum Art Hotel",
-			"place_id": "ChIJPUUrP9CvOG0R0Q2eyhDNe2g",
-			"position": {
-				"lat": -25.363, 
-				"lng": 131.044
-			}
-		}, {
-			"name": "Museum of New Zealand Te Papa Tongarewa",
-			"place_id": "ChIJnRaYrdGvOG0RMJot6PyfQJo",
-			"position": {
-				"lat": -41.290404, 
-				"lng": 174.782092
-			}
-		}, {
-			"name": "St James Theatre",
-			"place_id": "ChIJHX9AddqvOG0RjRLfnBMstG4",
-			"position": {
-				"lat": -41.292938, 
-				"lng": 174.779664
-			}
-		}, {
-			"name": "Embassy Theatre",
-			"place_id": "ChIJ41LdS8WvOG0RCsV2BBbXH1A",
-			"position": {
-				"lat": -41.294111, 
-				"lng": 174.784015
-			}
-		}, {
-			"name": "Hotel St George",
-			"place_id": "ChIJP2MAP9avOG0RBzRY4hOj7A0",
-			"position": {
-				"lat": -41.289008, 
-				"lng": 174.774589
-			}
-		}, {
-			"name": "Bodega",
-			"place_id": "ChIJ46za_tevOG0RxIvfK4qnTjo",
-			"position": {
-				"lat": -41.292485, 
-				"lng": 174.773183
-			}
-		}, {
-			"name": "Amora Hotel Wellington",
-			"place_id": "ChIJE6zu0NCvOG0RW__QOaEimhU",
-			"position": {
-				"lat": -41.290809, 
-				"lng": 174.778961
-			}
-		}
-]
+var attractions = [];
+
+var Attraction = function(name,url,lat,lng,address,city,country,rating) {
+	this.name = name;
+	this.url = url;
+	this.lat = lat;
+	this.lng = lng;
+	this.address = address;
+	this.city = city;
+	this.country = country;
+	this.rating = rating;
+}
 
 function ViewModel() {
 	var self = this;
 	var map;
 	var mapLocation;
-
+	var foursquareClientId = "K0R1ICQPTNSLDHJMIGCS3BJ4WDBLWEWWI5CJ0GANLJR3H3NY";
+	var foursquareClientSecret = "OCVJ4QCILOMGPVH5EPOAL1BV1EEIDIVIVJN4UKJ4UA5ZKQ1Q";
+	var foursquareDate = "20160110"
+	var foursquareLocation = 'Wellington, NZ'
+	var foursquareSection = "topPicks";
+	var foursquareUrl = "https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=" + foursquareClientId + "&client_secret=" + foursquareClientSecret + "&v=" + foursquareDate + "&near=" + foursquareLocation + "&section=" + foursquareSection;
+	var foursquareVenues = [];
 	self.markerList = ko.observableArray([]);
 	self.matchedMarkers = ko.observableArray([]);
 	self.query = ko.observable('').extend({
@@ -73,56 +39,60 @@ function ViewModel() {
     		} else {marker.setMap(map)} 
     		return result;
     	});
-
     	return resultsList;
   	});
+	
+	var infowindow;
+	self.setInfoWindow = function(clickedattraction) {
+		console.log(clickedattraction);
+		var index = self.markerList.indexOf(clickedattraction);
+		console.log(index);
+		infowindow.open(map, self.markerList()[index]);
+		infowindow.setContent(self.markerList()[index].title);
+
+	} 	
 
 	function initialize() {
 		mapLocation = new google.maps.LatLng(-41.2912176, 174.7784507);
 	    map = new google.maps.Map(document.getElementById('map'), {
-	      center: mapLocation,
-	      zoom: 14,
+	    	center: mapLocation,
+	    	zoom: 14
 	    });
+	    infowindow = new google.maps.InfoWindow({});
 
-	    initialMarkers.forEach(function(markerItem){
+	    $.getJSON(foursquareUrl, function(data) {
+	    	
+	    	data.response.groups[0].items.forEach(function (item){
+	    		foursquareVenues.push(item.venue);
+	    	})
 
-			var marker = new google.maps.Marker({
-	          position: new google.maps.LatLng(markerItem.position),
-	          map: map,
-	          title: markerItem.name
+	    	foursquareVenues.forEach(function(venue) { 
+	   			var attraction = new Attraction(venue.name,venue.url,venue.location.lat,venue.location.lng,venue.location.address,venue.location.city,venue.location.country,venue.rating);	   		
+	   			attractions.push(attraction);
+	   		});
+
+	    	attractions.forEach(function(markerItem) {
+	    		var marker = new google.maps.Marker({
+	          	position: new google.maps.LatLng(markerItem.lat,markerItem.lng),
+	          	map: map,
+	          	title: markerItem.name
+
 			});
+	    	marker.addListener('click', function() {
+    			infowindow.open(map, marker);
+    			infowindow.setContent(marker.title);
+  			});
 
 			self.markerList.push(marker);
 			self.matchedMarkers.push(marker);
 
-	  	})
-
+	  		})
+	    });
 	}
+
+
+
 	google.maps.event.addDomListener(window, 'load', initialize); 
 }
-
-
-
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
-
-
-
-
-/*
-	  	var service = new google.maps.places.PlacesService(map);
-	  	service.getDetails({
-	    placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-	  	}, function(place, status) {
-	    if (status === google.maps.places.PlacesServiceStatus.OK) {
-	      var marker = new google.maps.Marker({
-	        map: map,
-	        position: place.geometry.location
-	      });
-	      google.maps.event.addListener(marker, 'click', function() {
-	        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-	          'Place ID: ' + place.place_id + '<br>' +
-	          place.formatted_address + '</div>');
-	        infowindow.open(map, this);
-	      });
-*/
